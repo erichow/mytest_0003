9ce6f2cf3f47112df547e16d76d7f879adb8d840
erichow <fire8119@gmail.com>
2023-11-25 12:59:23 +0800

    eslint@7 eslint-plugin-prettier@4 eslint-config-prettier@latest .pretterrc.js .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,11 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
 
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
+  /*
   rules: {
     "vue/max-attributes-per-line": [2, {
       "singleline": 10,
@@ -195,4 +196,5 @@ module.exports = {
     }],
     'array-bracket-spacing': [2, 'never']
   }
+  */
 }
